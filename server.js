4cec1a1840f58fc5d656e67374219c3030b91af4
Shelby Harner <shelby.harner@gmail.com>
2021-04-26 18:21:12 -0700

    work on correcting lab 7

--- server.js
+++ server.js
@@ -17,16 +17,14 @@ const PORT = process.env.PORT || 3001;
 // });
 
 app.get('/weather-data', (request, response) => {
-  try {
-      let city = weatherData.city_name;
-      let lat = weatherData.lat;
-      let lon = weatherData.lon;
-      let dailyForecast = weatherData.data.map(element => new DailyForecast(element.weather.description, element.valid_date));
-      console.log(weatherData.data);
-      response.json(dailyForecast)
-  } catch {
+  let city = weatherData.city_name;
+  let lat = weatherData.lat;
+  let lon = weatherData.lon;
+  let dailyForecast = weatherData.data.map(element => new DailyForecast(element.weather.description, element.valid_date));
+  console.log(weatherData.data);
+  response.json(dailyForecast).catch(error => {
     response.status(500).send('Internal error!')
-  }
+  })
 });
 
 // pass parameters from front end to back end - lat, lon, citySearched
